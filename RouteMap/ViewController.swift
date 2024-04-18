32af543ab41447cb22af5cf1d713e45b7c870325
Elaidzha1940 <eli080293@gmail.com>
2024-04-18 10:02:46 +0300

    Added MapKit / CoreLocation - Update ViewController

--- RouteMap/ViewController.swift
+++ RouteMap/ViewController.swift
@@ -8,12 +8,68 @@
 //  */
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
+    
+    let mapView: MKMapView = {
+        let mapView = MKMapView()
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        return mapView
+    }()
+    
+    let addAddressButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("add", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let routeButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("add", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let resetButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("add", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .cyan
+        setConstraints()
+        
+        addAddressButton.addTarget(self, action: #selector(addAddressButtonTapped), for: .touchUpInside)
+        routeButton.addTarget(self, action: #selector(routeButtonTapped), for: .touchUpInside)
+        routeButton.addTarget(self, action: #selector(resetButtonTapped), for: .touchUpInside)
+    }
+    
+    @objc func addAddressButtonTapped() {
+        print("Add")
+    }
+    
+    @objc func routeButtonTapped() {
+        print("Route")
+    }
+    
+    @objc func resetButtonTapped() {
+        print("Reset")
     }
 }
 
+extension ViewController {
+    func setConstraints() {
+        view.addSubview(mapView)
+        NSLayoutConstraint.activate([
+            mapView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
+        ])
+    }
+}
