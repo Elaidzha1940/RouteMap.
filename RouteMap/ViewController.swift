cf3f3eda99be723d305fc69e4e01b08df7996396
Elaidzha1940 <eli080293@gmail.com>
2024-04-18 09:28:04 +0300

    RouteMap

---
+++ RouteMap/ViewController.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: RouteMap
+//  File: ViewController.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 18.04.2024
+//
+//  */
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+    }
+}
+
